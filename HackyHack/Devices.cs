936845ae76e73802732fcf6fe30ae83b86131c0d
Pfhoenix <pfhoenix@gmail.com>
2014-08-02 20:54:13 -0600

    Initial work on network crawling

--- HackyHack/Devices.cs
+++ HackyHack/Devices.cs
@@ -2,7 +2,7 @@
 
 namespace HackyHack
 {
-	public enum EDeviceConnectionType { DCT_Generic, DCT_Client, DCT_Network }
+	public enum EDeviceConnectionType { DCT_Client, DCT_Network }
 	public enum EDeviceConnectionMedium { DCM_Physical, DCM_Wireless, DCM_Satellite, DCM_LOS }
 
 	// the base class for all connection-between-devices types
@@ -14,33 +14,44 @@ namespace HackyHack
 		public EDeviceConnectionMedium Medium;
 
 		// amount of theoretical maximum data bandwidth
-		public uint MaxBandwidth;
+		public uint MaxTotalBandwidth;
+		public uint MaxPerConnectionBandwidth;
 
 		// amount of current data usage
-		public uint CurBandwidth;
+		public uint CurTotalBandwidth;
 
 		// all the devices connected together by this connection
-		List<DeviceConnection> Connections;
-		public bool bMulticast;
+		readonly List<DeviceConnection> Connections;
+		public int MaxConnections;
 
 		// the host device for this connection
 		public Device Host;
 
+		// the unique update ID to prevent backtracking when crawls touch this connection
+		public uint UpdateID;
 
-		public DeviceConnection(Device h, EDeviceConnectionType t, EDeviceConnectionMedium m, uint mb, bool bM = false)
+
+		public DeviceConnection(Device h, EDeviceConnectionType t, EDeviceConnectionMedium m, uint mtb, uint mcb, int mc)
 		{
 			Connections = new List<DeviceConnection>();
 			Host = h;
 			Type = t;
-			MaxBandwidth = mb;
+			MaxTotalBandwidth = mtb;
+			MaxPerConnectionBandwidth = mcb;
 			Medium = m;
-			bMulticast = bM;
+			MaxConnections = mc;
 		}
-
 
 		public int GetNumConnections()
 		{
 			return Connections.Count;
+		}
+
+		public uint GetPerConnectionBandwidth()
+		{
+			if (Connections.Count == 0) return 0;
+
+			return CurTotalBandwidth / (uint)Connections.Count;
 		}
 
 		public bool IsConnectedTo(Device d)
@@ -55,8 +66,7 @@ namespace HackyHack
 		public bool CanConnectTo(DeviceConnection dc)
 		{
 			if (Medium != dc.Medium) return false;
-			if ((Type == EDeviceConnectionType.DCT_Network) && (dc.Type == EDeviceConnectionType.DCT_Client)) return false;
-			if ((Type == EDeviceConnectionType.DCT_Client) && (dc.Type == EDeviceConnectionType.DCT_Network)) return false;
+			if (Type != dc.Type) return false;
 
 			return true;
 		}
@@ -65,20 +75,69 @@ namespace HackyHack
 		public bool ConnectTo(Device d)
 		{
 			if (IsConnectedTo(d)) return true;
-			if (!bMulticast && (GetNumConnections() >= 1)) return false;
+			if (Connections.Count >= MaxConnections) return false;
+
+			DeviceConnection dc = d.FindConnectionFor(this);
+			if (dc == null) return false;
+
+			Connections.Add(dc);
 
 			return true;
 		}
 	}
 
 	// the base class for all devices
+	// NOTE :
+	//   No device should have multiple connections with the same type but different bandwidth ratings
+	//   This will prevent issues of players feeling like they need to optimize device connections on one device
 	public class Device
 	{
+		public bool bActive;
 		List<DeviceConnection> Connections;
+
+		public ECrawlOptions CrawlDescriptor;
+		public uint CrawlID;
 
 		public Device()
 		{
 			Connections = new List<DeviceConnection>();
+		}
+
+		#region Tests for connection possibility
+		public bool CanConnectTo(DeviceConnection other)
+		{
+			foreach (DeviceConnection dc in Connections)
+			{
+				if (dc.CanConnectTo(other)) return true;
+			}
+
+			return false;
+		}
+
+		public bool CanConnectTo(Device other)
+		{
+			foreach (DeviceConnection dc in Connections)
+			{
+				if (other.CanConnectTo(dc)) return true;
+			}
+
+			return false;
+		}
+		#endregion
+
+		public DeviceConnection FindConnectionFor(DeviceConnection other)
+		{
+			foreach (DeviceConnection dc in Connections)
+			{
+				if (dc.CanConnectTo(other) && (dc.MaxConnections < dc.GetNumConnections())) return dc;
+			}
+
+			return null;
+		}
+
+		public bool ConnectTo(Device other, DeviceConnection by)
+		{
+			return by.ConnectTo(other);
 		}
 	}
 
@@ -88,6 +147,60 @@ namespace HackyHack
 	{
 		public NetworkDevice()
 		{
+		}
+
+		public void UpdateBandwidth()
+		{
+		}
+	}
+
+	// the entry point to the network
+	// ALL networks have a DTE, and shutting it down closes off the entire network immediately
+	public class DTE : NetworkDevice
+	{
+		public DTE()
+		{
+			CrawlDescriptor = ECrawlOptions.DTEs;
+		}
+	}
+
+	public class Firewall : NetworkDevice
+	{
+		public Firewall()
+		{
+			CrawlDescriptor = ECrawlOptions.Firewalls;
+		}
+	}
+
+	public class IDS : NetworkDevice
+	{
+		public IDS()
+		{
+			CrawlDescriptor = ECrawlOptions.IDS;
+		}
+	}
+
+	public class Hub : NetworkDevice
+	{
+		public Hub()
+		{
+			CrawlDescriptor = ECrawlOptions.Hubs;
+		}
+	}
+
+	public class Switch : NetworkDevice
+	{
+		public Switch()
+		{
+			CrawlDescriptor = ECrawlOptions.Switches;
+		}
+	}
+
+	public class Router : NetworkDevice
+	{
+		public Router()
+		{
+			CrawlDescriptor = ECrawlOptions.Routers;
 		}
 	}
 }
---
+++ HackyHack/Network.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackyHack
+{
+	[Flags]
+	public enum ECrawlOptions : uint
+	{
+		None = 0,
+		// active or inactive options
+		Active = None << 1,
+		Inactive = Active << 1,
+		Active_Inactive = Active | Inactive,
+		// network devices
+		Routers = Inactive << 1,
+		Switches = Routers << 1,
+		Hubs = Switches << 1,
+		Traffic_Devices = Routers | Switches | Hubs,
+		DTEs = Hubs << 1,
+		Firewalls = DTEs << 1,
+		IDS = Firewalls << 1,
+		Network_Devices = Traffic_Devices | DTEs | Firewalls | IDS,
+		// customer devices
+		Clients = IDS << 1,
+		Servers = Clients << 1,
+		Printers = Servers << 1,
+		Phones = Printers << 1,
+		Customer_Devices = Clients | Servers | Printers | Phones,
+		// option for crawling over non-matching devices
+		TouchNonMatch = Phones << 1,
+		// option for starting crawl at DTEs instead of a specified device
+		StartDTEs = TouchNonMatch << 1,
+		// convenience options
+		All_Active = Active | Network_Devices | Customer_Devices,
+		All = Active_Inactive | Network_Devices | Customer_Devices,
+	}
+
+	public class Network
+	{
+		// this helps facilitate proper bandwidth allocation crawling
+		public readonly List<DTE> DTEs;
+		public readonly List<Device> Devices;
+
+		public delegate bool NetworkCrawlCallback(Device d);
+
+		uint CrawlID;
+
+		public Network()
+		{
+			DTEs = new List<DTE>(5);
+			Devices = new List<Device>(100);
+			CrawlID = 0;
+		}
+
+		public void NetworkCrawl(ECrawlOptions options, NetworkCrawlCallback callback, Device start = null)
+		{
+			CrawlID++;
+		}
+	}
+}
+
